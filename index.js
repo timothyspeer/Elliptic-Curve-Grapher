d47f002a53022da4162166c003d9f824208c58af
timothyspeer <timspeer@outlook.com>
2018-08-20 17:16:49 -0700

    Updated UI and drawing mechanism

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function main() {
 function resize_window() {
   var box = document.getElementById('graph-box');
   var canvas = document.getElementById('graph-area');
-  var m = Math.min(box.clientWidth, 660);
+  var m = Math.min(box.clientWidth, 560);
 
+  // Convert the width of graphing area to be a multiple of 20
   if ((m % 20) !== 0) {
     m -= (m % 20);
   }
@@ -16,22 +17,60 @@ function resize_window() {
   canvas.width = m;
   canvas.height = m;
 
-  draw_graph();
+  submit_graph();
 }
 
-function draw_graph() {
+function submit_graph() {
   const inputs = {
     a: Number(document.getElementById('a').value),
     b: Number(document.getElementById('b').value),
     xRange: Number(document.getElementById('x-range').value),
   };
+  if (check_inputs(inputs)) {
+    draw_graph(inputs);
+    save_inputs();
+  } else {
+    const saved_inputs = {
+      a: Number(document.getElementById('a').save),
+      b: Number(document.getElementById('b').save),
+      xRange: Number(document.getElementById('x-range').save),
+    };
+    draw_graph(saved_inputs);
+  }
+}
+
+function check_inputs(inputs) {
+  var a = document.getElementById('a');
+  var b = document.getElementById('b');
+  var xRange = document.getElementById('x-range');
+  var valid = true;
+
+  if (a.validationMessage !== '' || b.validationMessage !== '' || xRange.validationMessage !== '') {
+    valid = false;
+  }
+
+  document.getElementById('a-valid').innerHTML = a.validationMessage;
+  document.getElementById('b-valid').innerHTML = b.validationMessage;
+  document.getElementById('xRange-valid').innerHTML = xRange.validationMessage;
+
+  return valid;
+}
+
+function save_inputs() {
+  document.getElementById('a').save = document.getElementById('a').value;
+  document.getElementById('b').save = document.getElementById('b').value;
+  document.getElementById('x-range').save = document.getElementById('x-range').value;
+}
+
+function draw_graph(inputs) {
   var canvas = document.getElementById('graph-area');
   var context = canvas.getContext('2d');
-
+  console.log(inputs);
   context.clearRect(0, 0, canvas.width, canvas.height);
   draw_axis(canvas, context);
   draw_steps(canvas, context);
   draw_curve(canvas, context, inputs);
+  draw_curve_equation(inputs);
 }
 
 function draw_axis(canvas, context) {
@@ -84,7 +123,7 @@ function draw_steps(canvas, context) {
 function draw_curve(canvas, context, inputs) {
   var x = 0;
   var y = [null, null];
-  var curve_width = 1;
+  var curve_width = 2;
 
   while (x <= canvas.width) {
     context.fillStyle = 'green';
@@ -109,4 +148,8 @@ function calculate_curve(canvas, inputs, x) {
     return [y1, y2];
   }
   return [null, null];
+}
+
+function draw_curve_equation(inputs) {
+  document.getElementById('curve-equation').innerHTML = 'y<sup>2</sup> = x<sup>3</sup> + ' + inputs.a + 'x + ' + inputs.b;
 }
